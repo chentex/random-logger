72702fee9f513e07797a8be1695673111e7b123d
Vicente Zepeda Mas <chente.z.m@gmail.com>
2018-03-02 22:50:05 -0600

    added date to the beggining of the message in iso-8601

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,15 @@ do
    sleep $waitTime &
    wait $!
    instruction=$(shuf -i 0-4 -n 1)
-   d=`date`
+   d=`date --iso-8601=seconds`
    case "$instruction" in
-      "1") echo "ERROR ($d) something happened in this execution."
+      "1") echo "$d ERROR something happened in this execution."
       ;;
-      "2") echo "INFO ($d) takes the value and converts it to string."
+      "2") echo "$d INFO takes the value and converts it to string."
       ;;
-      "3") echo "WARN ($d) variable not in use."
+      "3") echo "$d WARN variable not in use."
       ;;
-      "4") echo "DEBUG ($d) first loop completed."
+      "4") echo "$d DEBUG first loop completed."
       ;;
    esac
 done
