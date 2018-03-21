dfed500659a3a7aa158d0bff15210bff39d66549
Vicente Zepeda Mas <chente.z.m@gmail.com>
2018-03-20 22:31:28 -0600

    Fixed error with date command in alpine

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ do
    sleep $waitTime &
    wait $!
    instruction=$(shuf -i 0-4 -n 1)
-   d=`date --iso-8601=seconds`
+   d=`date -Iseconds`
    case "$instruction" in
       "1") echo "$d ERROR something happened in this execution."
       ;;
